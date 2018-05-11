e5fcbb825ac87445b58aa9e5375ffe24a586be32
Dan <dannymaish80@gmail.com>
2018-05-11 17:09:09 +0300

    Add chnges made to html file

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,6 @@
 
 
 
-
-
-
 //backend
   function rolldice1() {
     var dice1= document.getElementById("dice1");
@@ -22,20 +19,27 @@
     status.innerHTML= diceTotal;
     if(d1 === d2) {
       double.innerHTML= " DOUBLE!, you get another turn";
+      $("#clickable").click(function() {
+        $(".kit").hide();
+      });
     }
+
   };
-  function rolldice2() {
+  function rolldice() {
     var dice3= document.getElementById("dice3");
     var dice4= document.getElementById("dice4");
-    var status= document.getElementById("status");
-    var double= document.getEleentById("double2");
+    var pitch= document.getElementById("pitch");
+    var double= document.getElementById("double2");
     var d3= Math.floor(Math.random()* 6)+ 1;
     var d4= Math.floor(Math.random()* 6)+ 1;
     var diceTotal= d3 + d4;
     dice3.innerHTML= d3;
     dice4.innerHTML= d4;
-    status.innerHTML= diceTotal;
-    if(d1 === d2) {
+    pitch.innerHTML= diceTotal;
+    if(d3 === d4) {
       double.innerHTML= " DOUBLE!, you get another turn";
+      $("#erase").click(function() {
+        $("#double2").hide();
+      });
     }
   };
